b02a4bebc24f66526aee89a2ec630e14bf60be92
IASP23 <alexanderpilataxi232002@gmail.com>
2023-09-07 22:25:33 -0500

    Animation.js

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -5,3 +5,21 @@ sr.reveal(".timer", {
   origin: "bottom",
   distance: "-100px",
 });
+
+sr.reveal(".cards", {
+  duration: 3000,
+  origin: "right",
+  distance: "50px",
+});
+
+sr.reveal(".music", {
+  duration: 3000,
+  origin: "left",
+  distance: "50px",
+});
+
+sr.reveal(".form", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "150px",
+});
